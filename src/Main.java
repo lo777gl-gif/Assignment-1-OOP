cf8faffde05ebb74e18906d5af535ce2fea5cd5c
Dainsleif <lo777gl@gmail.com>
2026-08-11 21:56:47 +0500

    Main was changed

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class MyApplication {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/source");
 
